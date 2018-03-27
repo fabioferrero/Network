b118a41d3b88677a3287866869a1f12cc2581813
Fabio Ferrero <f.ferrero@reply.it>
2018-03-27 09:23:59 +0200

    [Network] Code reorganization

--- Network/Network.swift
+++ Network/Network.swift
@@ -37,6 +37,9 @@ extension Network {
     }
 }
 
+// TODO: Service Extension
+// Add an extension to the `Service` protocol so that it can return a Request
+// ready with itself.
 protocol Service {
     static var url: String { get }
     associatedtype Input: Encodable
@@ -77,81 +80,6 @@ final class Network: NSObject {
     
     // Make the initializer private for the singleton
     private override init() { super.init() }
-}
-
-// MARK: - Session Delegate
-
-extension Network: URLSessionDelegate, URLSessionDataDelegate, URLSessionDownloadDelegate {
-    
-    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
-        DispatchQueue.main.async {
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
-                let completionHandler = appDelegate.backgroudSessionCompletionHandler {
-                appDelegate.backgroudSessionCompletionHandler = nil
-                completionHandler()
-            }
-        }
-    }
-    
-    /// This delegate method is called when session task is finished. Check for
-    /// presence of `error` object to decide if call was successful or not
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            httpResponses[task]?.error = error
-        } else if let data = dataBuffers[task] {
-            if data.count > 0 {
-                httpResponses[task]?.data = data
-            }
-            dataBuffers[task] = nil // Clean the buffer
-        }
-        DispatchQueue.main.async {
-            if let httpResponse = self.httpResponses[task] {
-                self.completionHandlers[task]?(httpResponse.data, httpResponse.urlResponse, httpResponse.error)
-            }
-        }
-    }
-    
-    /// This delegate method is called once when response is recieved. This is
-    /// the place where you can perform initialization or other related tasks
-    /// before start recieviing data from response
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-        httpResponses[dataTask]?.urlResponse = response
-        completionHandler(.allow)
-    }
-    
-    /// This delegate method is called when response data is recieved in chunks
-    /// or in one shot.
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        dataBuffers[dataTask]?.append(data)
-    }
-    
-    /// This delegate method id called when the `dowloadTask` has finished its
-    /// work of downloading. It wrote in `location` all dowloaded data.
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        do {
-            let data = try Data(contentsOf: location)
-            dataBuffers[downloadTask]?.append(data)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-}
-
-// MARK: - Privates
-
-extension Network {
-    
-    private func add(task: URLSessionTask, withRelatedCompletionHandler completion: @escaping CompletionHandler) {
-        httpResponses[task] = (nil, nil, nil)
-        dataBuffers[task] = Data()
-        completionHandlers[task] = completion
-    }
-    
-    private func remove(task: URLSessionTask) {
-        httpResponses[task] = nil
-        dataBuffers[task] = nil
-        completionHandlers[task] = nil
-    }
 }
 
 // MARK: - Calls
@@ -208,79 +136,81 @@ extension Network {
             callback(Response.KO(error: .encodingError(errorMessage: error.localizedDescription)))
         }
     }
+}
+
+// MARK: - Privates
+
+extension Network {
     
-    func callService<E, D>(withRequest request: Request<E>, callback: @escaping (_ response: Response<D>) -> Void) {
-        
-        guard let url = URL(string: request.serviceUrl) else {
-            callback(Response.KO(error: .invalidURL)); return
-        }
-        
-        do {
-            let data = try JSONEncoder().encode(request.payload)
-            
-            if let jsonString = String(data: data, encoding: .utf8) {
-                let log = "⬆️ REQUEST to: \(url)\n\(jsonString)"
-                print(log)
+    private func add(task: URLSessionTask, withRelatedCompletionHandler completion: @escaping CompletionHandler) {
+        httpResponses[task] = (nil, nil, nil)
+        dataBuffers[task] = Data()
+        completionHandlers[task] = completion
+    }
+    
+    private func remove(task: URLSessionTask) {
+        httpResponses[task] = nil
+        dataBuffers[task] = nil
+        completionHandlers[task] = nil
+    }
+}
+
+// MARK: - Session Delegate
+
+extension Network: URLSessionDelegate, URLSessionDataDelegate, URLSessionDownloadDelegate {
+    
+    // This delegate method is needed in order to reactivate the backgroud
+    // session when the app is not in foreground
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        DispatchQueue.main.async {
+            if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let completionHandler = appDelegate.backgroudSessionCompletionHandler {
+                appDelegate.backgroudSessionCompletionHandler = nil
+                completionHandler()
             }
-        
-            let httpRequest = NSMutableURLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: Constants.timeoutInterval)
-            httpRequest.httpMethod = "POST"
-            httpRequest.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-type")
-            httpRequest.httpBody = data // It is possible to encrypt this data here
-            
-            let downloadTask = backgroundSession.downloadTask(with: httpRequest as URLRequest)
-            
-            Network.shared.add(task: downloadTask, withRelatedCompletionHandler: { data, urlResponse, error in
-                defer { self.remove(task: downloadTask) }
-                
-                if let error = error {
-                    callback(Response.KO(error: .networkError(errorMessage: error.localizedDescription)))
-                } else {
-                    guard let data = data else {
-                        callback(Response.KO(error: .missingData)); return
-                    }
-                    
-                    if let jsonString = String(data: data, encoding: .utf8) {
-                        let log = "⬇️ RESPONSE from: \(url)\n\(jsonString)"
-                        print(log)
-                    }
-                    
-                    do {
-                        let response = try JSONDecoder().decode(D.self, from: data)
-                        callback(Response.OK(response: response))
-                    } catch {
-                        callback(Response.KO(error: .decodingError(errorMessage: error.localizedDescription)))
-                    }
-                }
-            })
-            
-            downloadTask.resume()
-        } catch {
-            callback(Response.KO(error: .encodingError(errorMessage: error.localizedDescription)))
         }
     }
     
-//    func callService<Decodable>(withURL url: URL, callback: @escaping (_ response: Response<Decodable>) -> Void) {
-//
-//        let dataTask = backgroundSession.dataTask(with: url)
-//
-//        Network.shared.add(task: dataTask, withRelatedCompletionHandler: { data, urlResponse, error in
-//            defer { self.remove(task: dataTask) }
-//
-//            if let error = error {
-//                callback(Response.KO(error: .networkError(errorMessage: error.localizedDescription)))
-//            } else {
-//                guard let data = data else { return }
-//
-//                do {
-//                    let response = try JSONDecoder().decode(Decodable.self, from: data)
-//                    callback(Response.OK(response: response))
-//                } catch {
-//                    callback(Response.KO(error: .decodingError(errorMessage: error.localizedDescription)))
-//                }
-//            }
-//        })
-//
-//        dataTask.resume()
-//    }
+    // This delegate method is called when session task is finished. Check for
+    // presence of `error` object to decide if call was successful or not
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            httpResponses[task]?.error = error
+        } else if let data = dataBuffers[task] {
+            if data.count > 0 {
+                httpResponses[task]?.data = data
+            }
+            dataBuffers[task] = nil // Clean the buffer
+        }
+        DispatchQueue.main.async {
+            if let httpResponse = self.httpResponses[task] {
+                self.completionHandlers[task]?(httpResponse.data, httpResponse.urlResponse, httpResponse.error)
+            }
+        }
+    }
+    
+    // This delegate method is called once when response is recieved. This is
+    // the place where you can perform initialization or other related tasks
+    // before start recieviing data from response
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        httpResponses[dataTask]?.urlResponse = response
+        completionHandler(.allow)
+    }
+    
+    // This delegate method is called when response data is recieved in chunks
+    // or in one shot.
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        dataBuffers[dataTask]?.append(data)
+    }
+    
+    // This delegate method id called when the `dowloadTask` has finished its
+    // work of downloading. It wrote in `location` all dowloaded data.
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        do {
+            let data = try Data(contentsOf: location)
+            dataBuffers[downloadTask]?.append(data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
