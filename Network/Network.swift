40a189a4f6dd900177376a584f28f5936deaec38
Fabio Ferrero <f.ferrero@reply.it>
2019-04-19 13:02:20 +0200

    [HTTPStatusCode] Implement HTTPStatusCode enum

--- Network/Network.swift
+++ Network/Network.swift
@@ -24,6 +24,28 @@ enum HTTPMethod: String {
     case patch
 }
 
+enum HTTPStatusCode: Int {
+    case ok                     = 200
+    case created                = 201
+    case accepted               = 202
+    case noContent              = 204
+    case movedPermanently       = 301
+    case found                  = 302
+    case seeOther               = 303
+    case notModified            = 304
+    case temporaryRedirect      = 307
+    case badRequest             = 400
+    case unauthorized           = 401
+    case forbidden              = 403
+    case notFound               = 404
+    case methodNotAllowed       = 405
+    case notAcceptable          = 406
+    case preconditionFailed     = 412
+    case unsupportedMedia       = 415
+    case internalServerError    = 500
+    case notImplemented         = 501
+}
+
 extension HTTPMethod: CustomStringConvertible {
     var description: String {
         return self.rawValue.uppercased()
