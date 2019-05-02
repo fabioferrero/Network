5a7f5340277dd636ed1dc117a53d62454782bfa5
Fabio Ferrero <f.ferrero@reply.it>
2019-05-02 15:15:33 +0200

    [Project] Remove commented code and remove unused FutureError file

--- Network/ViewController.swift
+++ Network/ViewController.swift
@@ -35,29 +35,13 @@ class ViewController: UIViewController {
         }
         
         loader.startAnimating()
-        URLSession.shared.request(for: GetRandomPhoto()).logged().transformed { data -> UIImage in
+        URLSession.shared.request(for: GetRandomPhoto()).transformed { data -> UIImage in
             if let image = UIImage(data: data) {
                 return image
             } else {
                 throw UIImage.Error.imageNotCreated
             }
-        }
-//        .observe { result in
-//            switch result {
-//            case .success(let image):
-//                DispatchQueue.main.async {
-//                    self.loader.stopAnimating()
-//                    self.imageView.image = image
-//                }
-//            case .failure(let error):
-//                DispatchQueue.main.async {
-//                    self.loader.stopAnimating()
-//                    let alert = Alert(title: "Error", message: error.localizedDescription)
-//                    alert.show(from: self)
-//                }
-//            }
-//        }
-        .onSuccess { photo in
+        }.onSuccess { photo in
             self.loader.stopAnimating()
             self.imageView.image = photo
         }.onFailure { error in
@@ -73,50 +57,50 @@ class ViewController: UIViewController {
 //        }
     }
     
-//    func callErrorService() {
-//        network.call(service: Services.errorService, input: ErrorService.Input()) { result in
-//            switch result {
-//            case .success:
-//                let alert = Alert(title: "Success", message: "It was actually a real success.")
-//                alert.show(from: self)
-//            case .failure(error: let error):
-//                let alert = Alert(title: "Error", message: error.localizedDescription)
-//                alert.show(from: self)
-//            }
-//        }
-//    }
-//
-//    private var backgroundPost: Post?
-//
-//    func callMyService() {
-//        let newPost = NewPost(userId: 3, title: "Title", body: "Body")
-//        loader.startAnimating()
-//        network.call(service: Services.createNewPost, input: newPost) { [weak self] result in
-//            guard let self = self else { return }
-//            self.loader.stopAnimating()
-//            switch result {
-//            case .success(let thePostThatYouWereWaitingFor):
-//                self.use(thePostThatYouWereWaitingFor)
-//                thePostThatYouWereWaitingFor.foo()
-//            case .failure(let error):
-//                let alert = Alert(title: "Error", message: error.localizedDescription)
-//                alert.show(from: self)
-//            }
-//        }
-//
-//        network.call(service: GetPhotosList.self, input: EmptyPayload(), onQueue: .background) { result in
-//            switch result {
-//            case .success: Logger.log(.info, message: "Success!")
-//            case .failure(let error):
-//                let alert = Alert(title: "Error", message: error.localizedDescription)
-//                alert.show(from: self)
-//            }
-//        }
-//    }
-//
-//    func use(_ post: Post) {
-//        Logger.log(.verbose, message: "Using: \(post)")
-//    }
+    func callErrorService() {
+        network.call(service: Services.errorService, input: ErrorService.Input()) { result in
+            switch result {
+            case .success:
+                let alert = Alert(title: "Success", message: "It was actually a real success.")
+                alert.show(from: self)
+            case .failure(error: let error):
+                let alert = Alert(title: "Error", message: error.localizedDescription)
+                alert.show(from: self)
+            }
+        }
+    }
+
+    private var backgroundPost: Post?
+
+    func callMyService() {
+        let newPost = NewPost(userId: 3, title: "Title", body: "Body")
+        loader.startAnimating()
+        network.call(service: Services.createNewPost, input: newPost) { [weak self] result in
+            guard let self = self else { return }
+            self.loader.stopAnimating()
+            switch result {
+            case .success(let thePostThatYouWereWaitingFor):
+                self.use(thePostThatYouWereWaitingFor)
+                thePostThatYouWereWaitingFor.foo()
+            case .failure(let error):
+                let alert = Alert(title: "Error", message: error.localizedDescription)
+                alert.show(from: self)
+            }
+        }
+
+        network.call(service: GetPhotosList.self, input: EmptyPayload(), onQueue: .background) { result in
+            switch result {
+            case .success: Logger.log(.info, message: "Success!")
+            case .failure(let error):
+                let alert = Alert(title: "Error", message: error.localizedDescription)
+                alert.show(from: self)
+            }
+        }
+    }
+
+    func use(_ post: Post) {
+        Logger.log(.verbose, message: "Using: \(post)")
+    }
 }
 
 extension UIImage {
